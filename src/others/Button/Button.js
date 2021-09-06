66d05aeb05561ea6a6bb0be6402d652b5cdc2c28
Nivea <nivea@respondeai.com.br>
2021-09-06 19:39:42 -0300

    preparando para o review

--- src/others/Button/Button.js
+++
@@ -1,7 +0,0 @@
-import "./style.css"
-
-export default function Button ({onclick, text}) {
-    return(
-        <button className="classic" onClick={onclick} >{text}</button>
-    )
-}
--- src/others/Title/Title.js
+++
@@ -1,7 +0,0 @@
-import "./style.css";
-
-export default function Title({text}) {
-    return(
-        <div className="title">{text}</div>
-    )
-}
